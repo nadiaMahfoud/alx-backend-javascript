e2faec7c65d55d28a27b8ac7af45c4b045540dfc
nadiaMahfoud <nadiamahfoud48@gmail.com>
2024-02-14 15:52:17 +0000

    task 11. Brand convention & Nominal typing

--- 0x04-TypeScript/js/main.ts
+++ 0x04-TypeScript/js/main.ts
@@ -1,68 +1,23 @@
-export interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'Dennis',
+  lastName: 'Ritchie',
+  experienceTeachingC: 10,
+};
 
-export interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
+console.log('C++');
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
 
-export class Director implements DirectorInterface {
-  workFromHome() {
-    return 'Working from home';
-  }
+console.log('Java');
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
 
-  getCoffeeBreak() {
-    return 'Getting a coffee break';
-  }
-
-  workDirectorTasks() {
-    return 'Getting to director tasks';
-  }
-}
-
-export class Teacher implements TeacherInterface {
-  workFromHome() {
-    return 'Cannot work from home';
-  }
-
-  getCoffeeBreak() {
-    return 'Cannot have a break';
-  }
-
-  workTeacherTasks() {
-    return 'Getting to work';
-  }
-}
-
-export function createEmployee(salary: (number | string)): (Director | Teacher) {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  }
-  return new Director();
-}
-
-export function isDirector(employee: (Director | Teacher)) {
-  return employee instanceof Director;
-}
-
-export function executeWork(employee: (Director | Teacher)) {
-  if (isDirector(employee)) {
-    return (employee as Director).workDirectorTasks();
-  }
-  return (employee as Teacher).workTeacherTasks();
-}
-
-export type Subjects = ('Math' | 'History');
-
-export function teachClass(todayClass: Subjects): string {
-  if (todayClass === 'Math') {
-    return 'Teaching Math';
-  }
-  if (todayClass === 'History') {
-    return 'Teaching History';
-  }
-}
+console.log('React');
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
