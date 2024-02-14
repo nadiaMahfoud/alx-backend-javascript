0c427b1952a3ef263b59753a0833329fcac0827d
nadiaMahfoud <nadiamahfoud48@gmail.com>
2024-02-14 15:01:57 +0000

    tasks from 1 to 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,36 +1,51 @@
-interface Teacher {
-  readonly firstName: string,
-  readonly lastName: string,
-  fullTimeEmployee: boolean,
-  yearsOfExperience?: number,
-  location: string,
-  [propName: string]: any,
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [index:string]: any;
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number,
+export interface Directors extends Teacher {
+  numberOfReports: number;
 }
 
-const printTeacher = (firstName:string, lastName:string) : string => `${firstName.charAt(0)}. ${lastName}`;
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
 
-class StudentClass {
-  firstName: string;
-  lastName: string;
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+export interface IStudentClassConstructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
 
   constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+    this._firstName = firstName;
+    this._lastName = lastName;
   }
 
-  workOnHomework = () : string => 'Currently working';
+  workOnHomework() {
+    return 'Currently working';
+  }
 
-  displayName = () : string => this.firstName;
+  displayName() {
+    return this._firstName;
+  }
 }
 
-interface StudentConstructor {
-  (firstName: string, lastName: string): classInterface;
+export function createStudent(ctor: IStudentClassConstructor, firstName: string, lastName: string): IStudentClass {
+  return new ctor(firstName, lastName);
 }
-
-const student = new StudentClass("Paul", "jerry");
-console.log(student.displayName())
-console.log(student.workOnHomework())
